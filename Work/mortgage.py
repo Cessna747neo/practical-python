b25377522ab11e420ab3cd5a52845ff0dfe8f7f5
Cessna747neo <140350439+Cessna747neo@users.noreply.github.com>
2023-07-27 19:41:36 -0400

    :yeee:

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,20 @@
-# mortgage.py
-#
-# Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extra_payment = 1000
+extra_payment_start = 61
+extra_payment_end = 108
+total_paid = 0.0
+month = 0
+
+while principal > 0:
+    if  month >= extra_payment_start and month <= extra_payment_end:
+        principal = principal * (1+rate/12) - payment - extra_payment
+        total_paid = total_paid + payment + extra_payment
+        
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    month = month + 1
+
+print('Total paid:', total_paid)
