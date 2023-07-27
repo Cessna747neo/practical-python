b9e9ecb4821a01ca4d9d0fed468c2d024a176790
Cessna747neo <140350439+Cessna747neo@users.noreply.github.com>
2023-07-27 19:58:59 -0400

    v2

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,6 +6,7 @@ extra_payment_start = 61
 extra_payment_end = 108
 total_paid = 0.0
 month = 0
+Num = 0
 
 while principal > 0:
     if  month >= extra_payment_start and month <= extra_payment_end:
@@ -16,5 +17,11 @@ while principal > 0:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
     month = month + 1
+    Num = Num + 1
+    
+    print(Num, total_paid)
 
-print('Total paid:', total_paid)
+if principal < 0:
+    total_paid = total_paid + principal
+    principal = 0
+    print(Num, total_paid, '(refunded from extra payment)')
