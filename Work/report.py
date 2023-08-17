c263e7e6a11dc595c6320c7a4067eff417288029
Cessna747neo <140350439+Cessna747neo@users.noreply.github.com>
2023-08-17 17:52:02 -0400

    a

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,13 @@
-# report.py
-#
-# Exercise 2.4
+import os
+import csv
+os.chdir('/Users/alex/practical-python/Work/Data/')
+
+def read_portfolio():
+    
+    portfolio = []
+
+    with open('portfolio.csv', 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
